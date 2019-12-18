ffb7284ee5463ff722a679b293d63816fbb9d65b
shwe-yuji <okumura19943210yuji@gmail.com>
2019-12-18 13:40:15 +0900

    変更しました

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,29 +1,59 @@
 $(function() {
-    function buildHTML(message){
-      if (message.image == null) {
-        let html = `<div class="message">
-                      <div class='message__name'> ${message.name}
-                      </div>
-                      <div class='message__date-time'> ${message.created_at}
-                      </div>
-                      <div class='message__comment'> ${message.body}
-                      </div>
-                    </div>`
-        return html;
+  
+  let buildHTML = function(message) {
+    let message_name = `<div class="message__name">` + message.name + `</div>`
+    let message_date_time = `<div class="message__date-time">` + message.created_at + `</div>`
+    let message_comment = `<div class='message__comment'>` + message.body + `</div>`
+    let message_image = `<img src="` + message.image + `" class="image" >`
 
-      } else if (message.image != null) { 
-        let html = `<div class="message">
-                      <div class='message__name'> ${message.name}
-                      </div>
-                      <div class='message__date-time'> ${message.created_at}
-                      </div>
-                      <div class='message__comment'> ${message.body}
-                        <img class='image' src=${message.image}/>
-                      </div>
-                    </div>`
-        return html;
-      }
+    if (message.image && message.body) {
+      let html = `<div class="message" data-message-id=` + message.id + `>
+                  ${message_name}
+                  ${message_date_time}
+                  ${message_comment}
+                  ${message_image}
+                  </div>`
+      return html;
+
+    } else if (message.body) { 
+      let html = `<div class="message" data-message-id=` + message.id + `>
+                    ${message_name}
+                    ${message_date_time}
+                    ${message_comment}
+                  </div>`
+      return html;
+
+    } else if (message.image) {
+      let html = `<div class="message" data-message-id=` + message.id + `>
+                    ${message_name}
+                    ${message_date_time}
+                    ${message_image}
+                  </div>`
+      return html;
     }
+  }
+
+  let reloadMessages = function(){
+    last_message_id = $('.message').last().data('messageId')
+    let url = $(location).attr('pathname').split('messages').join('');
+    $.ajax({
+      url: url + 'api/messages',
+      type: 'GET',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages){
+      let insertHTML = '';
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      $('.chat-space').append(insertHTML);
+      $('.chat-space').animate({scrollTop: $('.chat-space')[0].scrollHeight});
+    })
+    .fail(function(){
+        console.log('error!');
+      });
+    };
 
   $('#new_message').on('submit', function(e){
     e.preventDefault()
@@ -37,8 +67,9 @@ $(function() {
         processData: false,
         contentType: false
     })
-    .done(function(data){
-      let html = buildHTML(data);
+    .done(function(message) {
+      let html = buildHTML(message);
+      console.log(message)
       $('.chat-space').append(html);
       $('.chat-space').animate({scrollTop: $('.chat-space')[0].scrollHeight});
       $('#new_message')[0].reset();
@@ -48,4 +79,9 @@ $(function() {
       alert("メッセージの送信に失敗しました");
     });
   })
+
+  let present_url_path = $(location).attr('pathname');
+    if (present_url_path.includes('/groups/') && present_url_path.includes('/messages')) {
+      setInterval(reloadMessages, 7000);
+    }
 });
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -2,3 +2,5 @@ json.name @message.user.name
 json.body @message.body
 json.image @message.image.url
 json.created_at @message.created_at.strftime("%Y-%m-%d %H:%M")
+json.id @message.id
+
